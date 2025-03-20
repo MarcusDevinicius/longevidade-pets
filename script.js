48ef2a23353a0fc109be939dc7df1f0936aed06b
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-03-20 16:12:07 -0300

    Completando arquivo json.
    Arquivo json completo com idade dos cachorros de acordo com o peso, função para transformar número(1) em extenso(um) para assim acessar as propriedades do objeto através dos [] já que será passada por uma variável que por sua vez será a idade do animal capturada no formulário.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('[data-pet="form"]');
 const resultIdd = document.querySelector('[data-pet="resultado"]');
 const btnCalc = document.querySelector('[data-pet="btn-calc"]');
+const resultFinal = document.querySelector('[data-pet="resultado"]');
+
+
 
 class Pet {
     constructor(especie, idade, peso) {
@@ -8,41 +11,65 @@ class Pet {
         this.idade = idade;
         this.peso = peso;
     }
-    calcIdade(especie, idade, peso) {
-        console.log(idade);
-        if (especie === 'CACHORRO' && peso <= 10 && idade === 1) {
-            // console.log(`É um cachorro e tem Menos de 10kg`);
-            pegandoDados();
-           
+    buscarIdade(especie, idade, peso) {
+        if (especie === 'CACHORRO') {
+            pegandoDadosDog(idade, peso);
+
+        } else if (especie === 'GATO') {
+
         }
     }
 }
 
-// const MyPet = new Pet('Cachorro');
-// console.log(MyPet)
-
-btnCalc.addEventListener('click', handleClick);
+btnCalc.addEventListener('click',  handleClick);
 
 function handleClick() {
     const inputPet = form[0].value.toUpperCase();
-    const inputIddPet = +form[1].value;
+    let inputIddPet = +form[1].value;
     const inputPesoPet = +form[2].value;
+    inputIddPet = numeroParaExtenso(inputIddPet);
     const MyPet = new Pet(inputPet, inputIddPet, inputPesoPet);
-    MyPet.calcIdade(inputPet, inputIddPet, inputPesoPet);
-    console.log(MyPet);
-    // result.innerText = `É um ${inputPet} e tem ${inputIddPet} e ${inputPesoPet}`
+    MyPet.buscarIdade(inputPet, inputIddPet, inputPesoPet);
+    // console.log(MyPet);
+    // resultFinal.innerText = `É um ${inputPet} e tem ${inputIddPet} e ${inputPesoPet}`
 }
-function pegandoDados() {
+
+
+function pegandoDadosDog(idade, peso) {
     fetch('idades-pets.json').then(result => {
-        const jsonPets = result.json(); //promise, result: objsct
+        const jsonPets = result.json(); //promise, result: object
         jsonPets.then(objPets => { //object
-            const idadeH = objPets.dezmais.um;
-            console.log(idadeH);
-            resultIdd.innerText = `Em idade de humanos seu cachorro tem ${idadeH} anos.`
-            // console.log(jsonPets);
+            const jsonFile = objPets;
+            if(peso < 10) {
+                pesoPetHPeq = jsonFile.dezmenos[idade]
+                resultIdd.innerText = `Em idade de humanos seu Cachorro tem ${pesoPetHPeq} anos.`
+            } else if(peso >= 10 && peso <= 22) {
+                pesoPetHMed = jsonFile.dezevintedois[idade];
+                resultIdd.innerText = `Em idade de humanos seu Cachorro tem ${pesoPetHMed} anos.`
+            } else if (peso > 22) {
+                pesoPetHG = jsonFile.vintedoismais[idade];
+                resultIdd.innerText = `Em idade de humanos seu Cachorro tem ${pesoPetHG} anos.`
+            }
+            // console.log(idadeH);
+            // resultIdd.innerText = `Em idade de humanos seu Cachorro tem ${idadeH} anos.`
         })
     });
-}   
+}
+
+function numeroParaExtenso(numero) {
+    const unidades = ["", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"];
+    const menorQVinte = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"];
+    if (numero < 10) {
+        return unidades[numero]
+    }
+    else if (numero < 20){
+        return menorQVinte[numero - 10]
+    }
+    console.log(numero)
+}
 
 
+numeroParaExtenso();
 
+
+//Fazer tratamento de dados no formulário.
